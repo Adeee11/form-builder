8bff7c5e97ff3c6e85d36d1253951ed416116db6
nitskp <nitinkpandey4@gmail.com>
2022-03-01 18:57:24 +0530

    Typeform clone landing page done
    There are three issues. They are : 1.Making the list appear in bottom of get started button 2.See what was the problem in index.tsx export in case of description component 3.After closing the modal the page is refreshing automatically with form contents. Prevent that. 4. Also proper styling for navbar

--- client/src/components/LoginModal/LoginModal.tsx
+++ client/src/components/LoginModal/LoginModal.tsx
@@ -1,28 +1,39 @@
-import clsx from 'clsx';
-import { useState } from 'react';
-import {AiOutlineClose} from 'react-icons/ai'
-import './LoginModal.css'
+import clsx from "clsx";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import "./LoginModal.css";
 
 const LoginModal = () => {
-  const [openPopUp, setOpenPopup] = useState(false)
+  const [openPopUp, setOpenPopup] = useState(false);
 
   return (
-    <div className='login-modal'>
-      <button onClick={()=>setOpenPopup(!openPopUp)}>Log In</button>
-      <div className={clsx("login-modal__content", {"login-modal--show": openPopUp})}>
-      <button onClick={()=>setOpenPopup(!openPopUp)}>
-        <AiOutlineClose/>
-      </button>
-        <form action="">
+    <div className="login-modal">
+      <button onClick={() => setOpenPopup(!openPopUp)}>Log In</button>
+      <div
+        className={clsx("login-modal__content", {
+          "login-modal--show": openPopUp,
+        })}
+      >
+        <form >
+          <button
+            onClick={() => setOpenPopup(!openPopUp)}
+            className="login-modal__cross-button"
+          >
+            <AiOutlineClose />
+          </button>
           <h1>Log In</h1>
+           {/* User Name Field  */}
           <div className="login-modal__form-field">
             <label htmlFor="userID">User ID: </label>
             <input type="text" id="userID" name="userID" />
           </div>
+          {/* Password Field  */}
           <div className="login-modal__form-field">
             <label htmlFor="">Password</label>
             <input type="password" name="password" id="password" />
           </div>
+           {/* Submit Form Button  */}
+          <input type="submit"  value={'Log In'}/>
         </form>
       </div>
     </div>
--- client/src/components/SignUpModal/SignUpModal.tsx
+++ client/src/components/SignUpModal/SignUpModal.tsx
@@ -8,25 +8,34 @@ const SignUpModal = () => {
 
   return (
     <div className="signup-modal">
+      {/* Sign Up Button  */}
       <button onClick={() => setOpenPopup(!openPopUp)}>Sign Up</button>
+
+      {/* Modal Content  */}
       <div
         className={clsx(" signup-modal__content", {
           " signup-modal--show": openPopUp,
         })}
       >
-        <form action="">
-          <button onClick={() => setOpenPopup(!openPopUp)}>
+        <form >
+          {/* Cross Button  */}
+          <button onClick={() => setOpenPopup(!openPopUp)} className="signup-modal__cross-button">
             <AiOutlineClose />
           </button>
           <h1>Sign Up</h1>
+
+          {/* User Name Field  */}
           <div className=" signup-modal__form-field">
             <label htmlFor="userID">User ID: </label>
             <input type="text" id="userID" name="userID" />
           </div>
+          {/* Password Field  */}
           <div className=" signup-modal__form-field">
-            <label htmlFor="">Password</label>
+            <label htmlFor="">Password: </label>
             <input type="password" name="password" id="password" />
           </div>
+          {/* Submit Form Button  */}
+          <input type="submit"  value={'Sign Up'}/>
         </form>
       </div>
     </div>
